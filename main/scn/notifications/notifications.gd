3d21b81b1ee2d56b2f5d76bb1a78175b36e128b9
Nicolò Santilio <n.santilio97@gmail.com>
2021-04-16 18:53:40 +0200

    update v1.4.2

--- main/scn/notifications/notifications.gd
+++ main/scn/notifications/notifications.gd
@@ -13,7 +13,7 @@ func manage_notification(resource : FirebaseResource) -> void:
     notification_node.connect("header_pressed", Activities.home, "_on_show_user_profile")
     notification_node.connect("show_post", Activities.home, "_on_open_post")
     notifications_list.add_child(notification_node)
-    notifications_list.move_child(notification_node, 0)
+#    notifications_list.move_child(notification_node, 0)
     notification_node.load_notification(resource)
 
 func view_notifications() -> void:
