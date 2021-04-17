00b30bb1f9d016e4937d96fa59193df5a50f538b
Nicolò Santilio <n.santilio97@gmail.com>
2021-04-17 13:12:28 +0200

    update v1.4.5

--- main/scn/app/main.gd
+++ main/scn/app/main.gd
@@ -5,8 +5,7 @@ const version : String = "1.4"
 onready var activities : Control = $Main/Activities
 onready var topbar : HBoxContainer = $TopBar
 onready var loading : Control = $Main/Loading
-onready var error_container : AspectRatioContainer = $Main/ErrorContainer
-onready var error_lbl : Label = error_container.get_node("ERROR")
+onready var error_lbl : Label = $Main/ERROR
 
 onready var user_id_lbl : Label = $Main/AppInfo/UserId
 onready var version_lbl : Label = $Main/AppInfo/Version
@@ -27,6 +26,9 @@ func _ready():
             if child is Control: child.hide()
     if OS.get_name() in ["HTML5"]:
         topbar.hide()
+        get_parent().rect_position = Vector2(0,0)
+        get_parent().rect_size = OS.window_size
+        get_parent().rect_clip_content = true
     loading.set_loading(false)
     Activities.signin = Activities.signin_scene.instance()
     activities.add_child(Activities.signin)
@@ -48,17 +50,17 @@ func _on_signin_completed():
     activities.add_child(Activities.home)
 
 func _on_show_error(error : String):
-    error_container.set_size(Vector2())
+    error_lbl.set_size(Vector2())
     error_lbl.set_text(error)
-    animator.interpolate_property(error_container, "rect_position", 
-    Vector2(rect_size.x/2 - error_container.rect_size.x/2, rect_size.y + 10), 
-    Vector2(rect_size.x/2 - error_container.rect_size.x/2, rect_size.y - error_container.rect_size.y), 
+    animator.interpolate_property(error_lbl, "rect_position", 
+    Vector2(rect_size.x/2 - error_lbl.rect_size.x/2, rect_size.y + 10), 
+    Vector2(rect_size.x/2 - error_lbl.rect_size.x/2, rect_size.y - $Main.rect_position.y - error_lbl.rect_size.y - 20), 
     0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
     animator.start()
     yield(get_tree().create_timer(9), "timeout")
-    animator.interpolate_property(error_container, "rect_position", 
-    error_container.rect_position, 
-    Vector2(rect_size.x/2 - error_container.rect_size.x/2, rect_size.y + 10), 
+    animator.interpolate_property(error_lbl, "rect_position", 
+    error_lbl.rect_position, 
+    Vector2(rect_size.x/2 - error_lbl.rect_size.x/2, rect_size.y + 10), 
     0.3, Tween.TRANS_QUAD, Tween.EASE_OUT)
     animator.start()
 
@@ -75,11 +77,15 @@ func _on_TopBar_minimize():
 
 func _on_TopBar_resize():
     if OS.get_window_size().x <= 1024:
-        OS.set_window_size(OS.get_screen_size(0) - Vector2(0, 50))
+        get_parent().rect_position = Vector2(0,0)
         OS.set_window_position(Vector2.ZERO)
+        get_parent().rect_size+=Vector2(8,8)*2
+        OS.set_window_size(OS.get_screen_size(0) - Vector2(0, 50))
     else:
-        OS.set_window_size(Vector2(1024, 600))
+        get_parent().rect_position = Vector2(8,8)
         OS.set_window_position(OS.get_screen_size(0)/2 - OS.get_window_size()/2)        
+        get_parent().rect_size-=Vector2(8,8)*2
+        OS.set_window_size(Vector2(1024, 600))
 
 func _on_TopBar_moving_from_pos(event_pos : Vector2, offset : Vector2):
     if OS.get_window_size().x > 1024:
